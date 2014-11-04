e7da7f867fa61a20ae4cc23bdc5e37dd414da012
Miguel A. Moreno <miguel@tyba.com>
2014-11-04 15:08:08 +0100

    Setting UICollectionDataSource as in the course

---
+++ ExchangeAGram-ios8/FeedViewController.swift
@@ -0,0 +1,51 @@
+//
+//  FeedViewController.swift
+//  ExchangeAGram-ios8
+//
+//  Created by Miguel Angel Moreno Armenteros on 04/11/14.
+//  Copyright (c) 2014 Miguel Angel Moreno Armenteros. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView:UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
